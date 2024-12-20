00822730eddcb740fbc51b58ee988eebe36ff734
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-12-20 20:44:04 -0300

    Exercicio Finalizado

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Produto> produtos = Arrays.asList(
+                new Produto("Smartphone", 800.0, "Eletrônicos"),
+                new Produto("Video Game", 3500.0, "Eletrônicos"),
+                new Produto("Teclado", 200.0, "Eletrônicos"),
+                new Produto("Cadeira", 300.0, "Móveis"),
+                new Produto("Monitor", 900.0, "Eletrônicos"),
+                new Produto("Mesa", 700.0, "Móveis")
+        );
+
+        List<Produto> produtosFiltrados = produtos.stream()
+                .filter(p -> p.getCategoria().equalsIgnoreCase("Eletrônicos"))
+                .sorted(Comparator.comparing(Produto::getPreco))
+                .collect(Collectors.toList());
+
+        produtosFiltrados.forEach(System.out::println);
+    }
+}
